fd6b9036daecee1d029be5feab5c99414992b9dd
truongha <truongxuanha882@gmail.com>
2023-02-28 00:17:08 +0700

    php

---
+++ connectDB.php
@@ -0,0 +1,19 @@
+<?php
+function connectDB()
+{
+  $server = "localhost";
+  $user = "root";
+  $pass = "";
+  $database = "btth01_cse485_ex.sql";
+  $conn = new mysqli(
+    $server,
+    $user,
+    $pass,
+    $database
+  );
+  $conn->set_charset("utf8");
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  return $conn;

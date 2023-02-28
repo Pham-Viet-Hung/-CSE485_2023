6846d536015da6ab402a3e6577764b9827a76269
Pham-Viet-Hung <104444637+Pham-Viet-Hung@users.noreply.github.com>
2023-02-28 10:23:00 +0700

    Add files via upload

--- header.php
+++ header.php
@@ -2,24 +2,21 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-  <title>CSE2023</title>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="./css/style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Music for Life</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css/style_login.css">
 </head>
 
 <body>
-
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
       <div class="container-fluid">
-        <div class="my-logo">
-          <a class="navbar-brand" href="#">
-            <img src="./images/logo2.jpg" alt="" class="img-fluid">
-          </a>
+        <div class="h3">
+          <a class="navbar-brand" href="index.php">Administration</a>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -27,16 +24,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./">Trang chủ</a>
+              <a class="nav-link active fw-bold" aria-current="page" href="./">Trang chủ</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./login.php">Đăng nhập</a>
+              <a class="nav-link" href="../index.php">Trang ngoài</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="category.php">Thể loại</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="author.php">Tác giả</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="article.php">Bài viết</a>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Tìm</button>
-          </form>
         </div>
       </div>
     </nav>

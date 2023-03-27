ac1e50a6445fba7fb70a9d6943755869a15673b3
truongxuanha <104482341+truongxuanha@users.noreply.github.com>
2023-03-27 10:17:08 +0700

    Delete handle_add_category.php

--- handle_add_category.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require '../connectDB.php';
-$conn = connectDB();
-$value =  $_REQUEST['valueCategory'];
-$query = "INSERT INTO `theloai` VALUES ('','$value');";
-$result = mysqli_query($conn, $query);
-header("Location: http://localhost/CSE485/admin/category.php");
